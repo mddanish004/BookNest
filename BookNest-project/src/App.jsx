77e5f786b029f03c539c39f232d1a13a19ed4c0d
Md Danish Ansari <m.danishansari004@gmail.com>
2024-06-17 13:05:06 +0530

    minor changes

--- BookNest-project/src/App.jsx
+++ BookNest-project/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Routes>
+
+      </Routes>
     </>
   )
 }
--- BookNest-project/src/main.jsx
+++ BookNest-project/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { FirebaseProvider } from "./context/Firebase.jsx"
 import './index.css'
+import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BroswerRouter>
     <FirebaseProvider>
     <App />
     </FirebaseProvider>
+    </BroswerRouter>
   </React.StrictMode>,
 )
